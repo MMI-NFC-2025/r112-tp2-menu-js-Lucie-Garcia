bc2f6ab9ee9f670bf12f0833c30179f85441ab8f
Lucie-Garcia <lucie.garcia05@edu.univ-fcomte.fr>
2025-11-12 15:07:39 +0100

    début js

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+// On cible les éléments à modifier
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+
+// Ajoute un écouteur d'événements à l'élément 'toggle' qui écoute les événements 'click'.
+toggle.addEventListener("click", () => {
+  // Mise à jour des attributs ARIA pour accessibilité
+  nav.setAttribute("aria-hidden"= "false");
+  toggle.setAttribute("aria-expanded" = "false");
+});
